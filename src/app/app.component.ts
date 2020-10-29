368ee8eb532866b2418b11b8121bf181fff859c2
Luis Espinoza <luis.espinoza.fr@viaexperis.pe>
2020-10-29 08:30:31 -0500

    qr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarcodeFormat } from '@zxing/library';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'qr1';
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo = null;
+  deviceCurrent: MediaDeviceInfo;
+  deviceSelected: string;
+  formatsEnabled: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.QR_CODE,
+  ];
+  hasDevices: boolean;
+  hasPermission: boolean;
+
+  qrResultString: string;
+
+  torchEnabled = false;
+  torchAvailable$ = new BehaviorSubject<boolean>(false);
+  tryHarder = false;
+
+  clearResult(): void {
+    this.qrResultString = null;
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
+  }
+
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+  }
+
+  // onDeviceSelectChange(selected: string) {
+  //   const device = this.availableDevices.find(x => x.deviceId === selected);
+  //   this.currentDevice = device || null;
+  // }
+
+  // openFormatsDialog() {
+  //   const data = {
+  //     formatsEnabled: this.formatsEnabled,
+  //   };
+
+  //   this._dialog
+  //     .open(FormatsDialogComponent, { data })
+  //     .afterClosed()
+  //     .subscribe(x => { if (x) { this.formatsEnabled = x; } });
+  // }
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+  }
+
+  // openInfoDialog() {
+  //   const data = {
+  //     hasDevices: this.hasDevices,
+  //     hasPermission: this.hasPermission,
+  //   };
+
+  //   this._dialog.open(AppInfoDialogComponent, { data });
+  // }
+
+  onTorchCompatible(isCompatible: boolean): void {
+    this.torchAvailable$.next(isCompatible || false);
+  }
+
+  toggleTorch(): void {
+    this.torchEnabled = !this.torchEnabled;
+  }
+
+  toggleTryHarder(): void {
+    this.tryHarder = !this.tryHarder;
+  }
 }
